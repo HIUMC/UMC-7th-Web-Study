227f13d46cb6d4e7a493326a8622407fce157a42
jade <kmj9151@naver.com>
2024-10-09 16:13:48 +0900

    2주차 미션2(1)

--- 2주차/2주차 미션2/src/App.jsx
+++ 2주차/2주차 미션2/src/App.jsx
@@ -4,13 +4,13 @@ import { MOVIES } from "./movies";
 
 function App() {
   return (
-    <div>
+    <>
       <div className="app-container">
         {MOVIES.results.map((movie) => {
           return <Movie key={movie.id} poster_path={movie.poster_path} />;
         })}
       </div>
-    </div>
+    </>
   );
 }
 
