8953ee6c2721d8d22aab9ded78c9a26b1b468e71
장자윤 <jayoonchang@naver.com>
2024-10-30 15:16:22 +0900

    use styled-components

--- 3주차/카테고리/src/components/Movie.jsx
+++ 3주차/카테고리/src/components/Movie.jsx
@@ -1,19 +1,51 @@
+import styled from "styled-components";
+
+const MovieContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: flex-start;
+  margin: 0 auto;
+`;
+const Img = styled.img`
+  width: 200px;
+  height: auto;
+  border-radius: 10px;
+`;
+const EachMovie = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+`;
+const MovieTitle = styled.span`
+  display: block;
+
+  margin-top: 5px;
+
+  font-size: 90%;
+  font-weight: bold;
+`;
+const MovieReleaseDate = styled.span`
+  display: block;
+  font-size: 50%;
+`;
+
 const Movie = ({ movies }) => {
   return (
-    <div className="movie-container">
+    <MovieContainer>
       {movies.map((movie) => (
-        <div key={movie.id} className="movie">
-          <img
+        <EachMovie key={movie.id}>
+          <Img
             src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
             alt={movie.title}
           />
           <div className="movie-info">
-            <span className="movie-title">{movie.title}</span>
-            <span className="movie-release-date">{movie.release_date}</span>
+            <MovieTitle>{movie.title}</MovieTitle>
+            <MovieReleaseDate>{movie.release_date}</MovieReleaseDate>
           </div>
-        </div>
+        </EachMovie>
       ))}
-    </div>
+    </MovieContainer>
   );
 };
 
