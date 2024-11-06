44c4273a3b537791c5b6f78c91cbcd7a28ec1c12
jade <kmj9151@naver.com>
2024-11-06 17:59:08 +0900

    .

--- 2주차/2주차 미션2/src/App.jsx
+++ 2주차/2주차 미션2/src/App.jsx
@@ -1,6 +1,18 @@
 import "./App.css";
 import Movie from "./components/Movie";
-import { MOVIES } from "./movies";
+import { MOVIES } from "./mocks/movies";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>홈 페이지입니다.</h1>,
+  },
+  {
+    path: "/movies",
+    element: <h1>영화 페이지 입니다.</h1>,
+  },
+]);
 
 function App() {
   return (
