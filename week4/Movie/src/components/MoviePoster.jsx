eddef481d495b0ea15e1bbe72ab96eafdc96f270
dkfla <arim329@naver.com>
2024-11-06 15:06:41 +0900

    4주차 과제

---
+++ week4/Movie/src/components/MoviePoster.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+
+const PosterContainer = styled.div`
+    position: relative;
+    display: inline-block;
+    margin: 5px;
+`;
+
+const PosterImage = styled.img`
+    width: 100px;
+    height: auto;
+    transition: 0.3s;
+    border-radius: 10px;
+`;
+
+const Overlay = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 150px;
+    border-radius: 10px;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+    transition: opacity 0.3s;
+    ${PosterContainer}:hover & {
+        opacity: 1;
+    }
+`;
+
+const MovieInfo = styled.div`
+    position: flex;
+    color: white;
+`;
+
+const MoviePoster = ({movies}) => {
+    return (
+        <>
+            {movies.map(movie => (
+                <Link to={`/movies/${movie.id}`} key={movie.id}>
+                    <PosterContainer>
+                        <PosterImage src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                        <Overlay />
+                        <MovieInfo>
+                            <h3>{movie.title}</h3>
+                            <p>{movie.release_date}</p>
+                        </MovieInfo>
+                    </PosterContainer>
+                </Link>
+            ))}
+        </>
+    );
+};
+
+export default MoviePoster;
---
+++ week4/Movie/src/pages/Movies.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import MoviePoster from '../components/MoviePoster';
+
+const CategoryContainer = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+`;
+
+const CategoryBoxes = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const CategoryBox = styled(Link)`
+  display: inline-block;
+  width: 200px;
+  height: 100px;
+  margin: 10px;
+  padding: 20px;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  text-align: center;
+  text-decoration: none;
+  color: black;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
+`;
+
+
+const Movies = () => {
+  const { category } = useParams();
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async (category) => {
+    const API_KEY = 'c77007945882250597394ca382111921';
+    let apiUrl;
+
+    switch (category) {
+      case 'popular':
+        apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR`;
+        break;
+      case 'now-playing':
+        apiUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=ko-KR`;
+        break;
+      case 'top-rated':
+        apiUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=ko-KR`;
+        break;
+      case 'up-coming':
+        apiUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=ko-KR`;
+        break;
+      default:
+        return;
+    }
+
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.error('Failed to fetch movies:', error);
+      setMovies([]); // 에러 발생 시 빈 배열로 설정
+    } 
+  };
+
+  useEffect(() => {
+    if (category) {
+      fetchMovies(category);
+    } else {
+      setMovies([]); // 카테고리 없이 페이지가 열릴 때 영화 데이터 초기화
+    }
+  }, [category]);
+
+  return (
+    <CategoryContainer>
+      {category === undefined && (
+        <>
+          <Title>카테고리</Title>
+          <CategoryBoxes>
+            <CategoryBox to="/movies/category/now-playing">현재 상영중인</CategoryBox>
+            <CategoryBox to="/movies/category/popular">인기있는</CategoryBox>
+            <CategoryBox to="/movies/category/top-rated">높은 평가를 받은</CategoryBox>
+            <CategoryBox to="/movies/category/up-coming">개봉 예정중인</CategoryBox>
+          </CategoryBoxes>
+        </>
+      )}
+      <MoviePoster movies={movies}/>
+    </CategoryContainer>
+  );
+};
+
+export default Movies;
